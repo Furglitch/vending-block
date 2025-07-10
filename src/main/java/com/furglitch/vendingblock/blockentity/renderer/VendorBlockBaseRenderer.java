9ec7ff02103bc6434dbef96d247d329b24dc0316
Furglitch <samsonger@proton.me>
2025-07-09 20:14:43 -0500

    Fix Z-Fighting on west face

--- src/main/java/com/furglitch/vendingblock/blockentity/renderer/VendorBlockBaseRenderer.java
+++ src/main/java/com/furglitch/vendingblock/blockentity/renderer/VendorBlockBaseRenderer.java
@@ -72,10 +72,10 @@ public class VendorBlockBaseRenderer {
         addVertex(matrix, vertexConsumer, x1, y1, z2, uMin, vMaxStretched, packedLight, packedOverlay, red, green, blue);
         
         // West
-        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH, y1, z2, uMax, vMaxStretched, packedLight, packedOverlay, red, green, blue);
-        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH, y2, z2, uMax, vMinStretched, packedLight, packedOverlay, red, green, blue);
-        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH, y2, z1, uMin, vMinStretched, packedLight, packedOverlay, red, green, blue);
-        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH, y1, z1, uMin, vMaxStretched, packedLight, packedOverlay, red, green, blue);
+        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH * 2, y1, z2, uMax, vMaxStretched, packedLight, packedOverlay, red, green, blue);
+        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH * 2, y2, z2, uMax, vMinStretched, packedLight, packedOverlay, red, green, blue);
+        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH * 2, y2, z1, uMin, vMinStretched, packedLight, packedOverlay, red, green, blue);
+        addVertex(matrix, vertexConsumer, x1 + TEXTURE_STRETCH * 2, y1, z1, uMin, vMaxStretched, packedLight, packedOverlay, red, green, blue);
 
         // East
         addVertex(matrix, vertexConsumer, x2, y1, z1, uMax, vMaxStretched, packedLight, packedOverlay, red, green, blue);
